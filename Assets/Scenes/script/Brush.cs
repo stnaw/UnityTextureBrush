d53470b81d1964bc351273129f69429dcc8ffc5a
stnaw <>
2021-01-19 14:19:08 +0800

    first commit

---
+++ Assets/Scenes/script/Brush.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+struct BrushJob : IJobParallelFor
+{
+    [ReadOnly] public NativeArray<Color32> ReadColors;
+    [WriteOnly] public NativeArray<Color32> WriteColors;
+    [ReadOnly]  public Vector4 PaintRange;
+    [ReadOnly]  public Vector2Int SourceTextureSize;
+    [ReadOnly]  public Vector2 CenterPoint;
+    Vector2 clamp(Vector2 min, Vector2 max, Vector2 current)
+    {
+        current.x = Mathf.Max(min.x, Mathf.Min(max.x, current.x));
+        current.y = Mathf.Max(min.y, Mathf.Min(max.y, current.y));
+        return current;
+    }
+    public void Execute(int index)
+    {
+        int row = index / SourceTextureSize.x;
+        int col = index % SourceTextureSize.x;
+        var max = 50f;
+
+        if(row > PaintRange.y && row < PaintRange.w && col > PaintRange.x && col < PaintRange.z)
+        {
+            var distance = Vector2.Distance(new Vector2(col, row), CenterPoint);
+            Color32 org = ReadColors[index];
+            org = distance > max ? org : new Color32(255, 0, 0, 255);
+            WriteColors[index] = org;
+        }
+    }
+}
+
+public class Brush
+{
+    private Texture2D targetTexture = null;
+    private NativeArray<Color32> targetTextureColors;
+
+    private Vector2 texSize = Vector2.zero;
+    public Brush(Texture2D targetTex)
+    {
+        targetTexture = targetTex;
+        targetTextureColors = targetTex.GetRawTextureData<Color32>();
+        texSize.x = targetTex.width;
+        texSize.y = targetTex.height;
+    }
+
+    Vector2 clamp(Vector2 min, Vector2 max, Vector2 current)
+    {
+        current.x = Mathf.Max(min.x, Mathf.Min(max.x, current.x));
+        current.y = Mathf.Max(min.y, Mathf.Min(max.y, current.y));
+        return current;
+    }
+
+    public void Draw(Vector2 point)
+    {
+        // this.draw_jobsystem(point);
+        this.draw_cpu(point);
+    }
+
+    private void draw_jobsystem(Vector2 point)
+    {
+        Vector2 brushSize = new Vector2(100, 100);
+        Vector2 start = clamp(Vector2.zero, texSize, point - brushSize * 0.5f);
+        Vector2 end = clamp(Vector2.zero, texSize, point + brushSize * 0.5f);
+        NativeArray<Color32> temp = new NativeArray<Color32>(targetTextureColors, Allocator.TempJob);
+        var job = new BrushJob()
+        {
+            ReadColors = temp,
+            WriteColors = targetTextureColors,
+            PaintRange = new Vector4(start.x, start.y, end.x, end.y),
+            SourceTextureSize = new Vector2Int((int)texSize.x, (int)texSize.y),
+            CenterPoint = point
+        };
+        var job_handle = job.Schedule(targetTextureColors.Length, 64);
+        job_handle.Complete();
+        temp.Dispose();
+    }
+
+    private void draw_cpu(Vector2 point)
+    {
+        Vector2 brushSize = new Vector2(50, 50);
+        Vector2 start = clamp(Vector2.zero, texSize, point - brushSize * 0.5f);
+        Vector2 end = clamp(Vector2.zero, texSize, point + brushSize * 0.5f);
+        var max = 25f;
+
+        for (int i = (int)start.x; i < end.x; i++)
+        {
+            for (int j = (int)start.y; j < end.y; j++)
+            {
+                var distance = Vector2.Distance(new Vector2(i, j), point);
+                int index = j * targetTexture.width + i;
+                Color32 org = targetTextureColors[index];
+                org = distance > max ? org : new Color32(255, 0, 0, 255);
+                targetTextureColors[index] = org;
+            }
+        }
+    }
+
+    public void Apply()
+    {
+        targetTexture.Apply();
+    }
+
+
+
+}
---
+++ Assets/Scenes/script/TextureBrushDemo.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureBrushDemo : MonoBehaviour
+{
+
+    Texture2D texture = null;
+    // Start is called before the first frame update
+    Brush brush = null;
+    Vector2 screenSize ;
+    void Start()
+    {
+        screenSize = new Vector2(Screen.width, Screen.height);
+        var camera = this.GetComponent<Camera>();
+        camera.aspect = screenSize.x / screenSize.y;
+        camera.orthographicSize = screenSize.y * 0.5f;
+        var bg = this.transform.GetChild(0);
+        bg.localScale = new Vector3(screenSize.x, screenSize.y, 1.0f);
+        var bg_m = bg.GetComponent<MeshRenderer>().material;
+        texture = new Texture2D(Screen.width, Screen.height);
+        bg_m.mainTexture = texture;
+        brush = new Brush(texture);
+    }
+
+    private Vector2 _touch = new Vector2(-1, -1);
+
+    private Vector2 touch
+    {
+        get
+        {
+            return _touch;
+        }
+        set
+        {
+            if(_touch != value )
+            {
+                Vector2 old = _touch;
+                _touch = value;
+
+                if(old != new Vector2(-1, -1))
+                    this.OnTouchPositionChanged(old, _touch);
+            }
+        }
+    }
+
+    private void OnTouchPositionChanged(Vector2 oldPosition, Vector2 newPosition)
+    {
+        float dis = Vector2.Distance(Vector2.zero, newPosition - oldPosition);
+
+        if(dis > 0f)
+        {
+            for (float i = 0; i < dis; i += 10f)
+            {
+                brush.Draw(oldPosition + i * (newPosition - oldPosition).normalized);
+            }
+
+            brush.Apply();
+        }
+    }
+
+    private bool gestureStarted = false;
+    Vector3 position;
+
+    Vector2 GetTouch()
+    {
+        Vector3 pos = Input.mousePosition;
+        Vector2 position = new Vector2(Mathf.Max(0f, Mathf.Min(screenSize.x, pos.x)), Mathf.Max(0f, Mathf.Min(screenSize.y, pos.y)));
+        return position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Vector2 t = GetTouch();
+
+            if(!gestureStarted)
+            {
+                _touch = t;;
+                gestureStarted = true;
+            }
+            else
+            {
+                touch = t;
+            }
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            if(!gestureStarted) return;
+
+            if(gestureStarted)
+            {
+                touch = GetTouch();
+            }
+
+            _touch = new Vector2(-1, -1);
+            gestureStarted = false;
+        }
+    }
+}
